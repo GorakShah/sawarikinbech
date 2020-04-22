ac1754d87fb5e771845b8a1f47a6b12d3d2a007d
neptech4u <bhagyasah4u@gmail.com>
2020-04-22 22:16:14 +0545

    blueprint added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import '../node_modules/@blueprintjs/core/lib/css/blueprint.css';
+import '../node_modules/@blueprintjs/icons/lib/css/blueprint-icons.css';
 import './styles/css/index.css';
 import App from './App';
-
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
